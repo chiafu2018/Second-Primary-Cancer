625580f56e0d70ba5c85962f92c037c136383429
chiafu2018 <chiafu2018@gmail.com>
2025-02-19 15:51:02 +0800

    docs: utils folder

--- utils/count-distinct-elements.py
+++ utils/count-distinct-elements.py
@@ -3,12 +3,7 @@ import pandas as pd
 # Load CSV file into DataFrame
 df = pd.read_csv('SEER_en.csv')
 
-# df = pd.read_csv('D:\新增資料夾\Data_preprocessed_LOC.csv')
-
-
 # Select the column and then get unique values
-
-# column_name = ["SSF1","SSF2","AJCC", "LYMND","Radiation","Surgery"]
 column_name = ["Histologic Type ICD-O-3", 'Target']
 
 for col in column_name:
